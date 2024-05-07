efeddb9333242884ae1ed24ccaf0a2f330b0a483
Rohit Thakre <thake785@gmail.com>
2024-05-07 23:13:03 +0530

    signal added.

--- signal_app/models.py
+++ signal_app/models.py
@@ -7,3 +7,22 @@ class Blog(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_by')
+
+    def __str__(self) -> str:
+        return f"{self.title}_{self.author}"
+    
+
+class Author_Notification(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="user who created the blog")
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, help_text="blog created by the user")
+    action = models.CharField(max_length=10, help_text="action performed on the blog by some user, i.e. liked or disliked")
+    notification = models.CharField(max_length=100, help_text="notification content")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.notification
+    
+    
--- signal_app/signals.py
+++ signal_app/signals.py
@@ -1,11 +1,22 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from signal_app import models
-from signal_app import models
+
 from django.dispatch import receiver
 
-@receiver(post_save, sender=models.Blog)
-def blog_saved(sender, instance, created, **kwargs):
-    print("post_save singnal initialized")
-    # some active here when blog model in updated
+@receiver(m2m_changed, sender=models.Blog.liked_by.through)
+def blog_saved(sender, instance,action, **kwargs):
+    if action == "post_add":
+        user_action = "liked"
+    else: 
+        user_action = "unliked"
 
+    models.Author_Notification.objects.update_or_create(
+        user= instance.author,
+        blog_id = instance.id,
+        defaults={"action": action,
+                  "notification": 
+                  f"{instance.author.username} {user_action} your blog at {instance.updated_at}"})
+    
+
+  
     
